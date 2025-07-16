167a0539cd5006c8c8781a485e3811ff7c5fbc52
xdDhanusha <dhanushanarayana@gmail.com>
2025-07-16 04:52:50 +0000

    Initial recommender version

---
+++ main.py
@@ -0,0 +1,11 @@
+ 
+from recommender.outfit_recommender import OutfitRecommender 
+
+
+def main(): 
+  recommender = OutfitRecommender("data/outfits.csv") 
+  print("Recommendations for color='Red', occasion='Party':") 
+  print(recommender.recommend(color='Blue', occasion='Casual')) 
+
+if __name__ == "__main__": 
+  main() 
